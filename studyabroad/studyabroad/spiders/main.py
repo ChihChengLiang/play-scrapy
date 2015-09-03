33a29afd07e664ae2ca1622428b2073a93e84c93
ChihChengLiang <chihchengliang@gmail.com>
2015-09-03 18:24:51 +0800

    fix missing value bug

--- studyabroad/studyabroad/spiders/main.py
+++ studyabroad/studyabroad/spiders/main.py
@@ -12,7 +12,7 @@ class PTTSpider(scrapy.Spider):
         max_index_str = response.css("#action-bar-container .btn-group.pull-right a:nth-child(2)::attr(\"href\")").re("index(\d*)\.html")[0]
         max_index = int(max_index_str)
 
-        for index in range(max_index-1, max_index + 1):
+        for index in range(1, max_index + 1):
             yield scrapy.Request("http://www.ptt.cc/bbs/studyabroad/index%s.html" % index, self.parse_index_page)
 
     def parse_index_page(self, response):
@@ -25,9 +25,9 @@ class PTTSpider(scrapy.Spider):
                 item["uid"] = re.search(p0, link).group(1)
                 item["title"] = title
                 p = re.compile(ur'\[(.*)\]')
-                tag = re.search(p, title).group(1)
+                tag = re.search(p, title)
                 if tag:
-                    item["tag"] =tag
+                    item["tag"] =tag.group(1)
                 push_span = entry.css(".nrec span::text")
                 if len(push_span)>0:
                     item["total_push"] = push_span[0].extract()
@@ -39,10 +39,15 @@ class PTTSpider(scrapy.Spider):
         item = response.meta['item']
         item["raw"] = response.css("#main-content")[0].extract()
         item["content"] = response.css("#main-content::text")[0].extract()
-        item["author"] = response.css(".article-meta-value::text")[0].extract()
-        d = response.css(".article-meta-value::text")[3].extract()
-        item["published"] = datetime.strptime(d, "%a %b\t%d %H:%M:%S %Y")
-        item["ip"] = response.css(".f2").re("(?:[0-9]{1,3}\.){3}[0-9]{1,3}")[0]
+        meta_value = response.css(".article-meta-value::text")
+        if len(meta_value)>0:
+            item["author"] = meta_value[0].extract()
+        elif len(meta_value)>=4:
+            d = response.css(".article-meta-value::text")[3].extract()
+            item["published"] = datetime.strptime(d, "%a %b\t%d %H:%M:%S %Y")
+        iptag = response.css(".f2").re("(?:[0-9]{1,3}\.){3}[0-9]{1,3}")
+        if len(iptag)>0:
+            item["ip"] = iptag[0]
         comments = []
         for push in response.css(".push"):
             p = {}
